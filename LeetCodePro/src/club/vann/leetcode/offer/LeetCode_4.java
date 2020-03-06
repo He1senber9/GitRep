ca72014de3c4e4a033fd1a862a24f5ed529ae30d
fanyun2011@aliyun.com <fanyun2011@aliyun.com>
2020-03-06 22:48:52 +0800

    leetcode_4 开发中

--- LeetCodePro/src/club/vann/leetcode/offer/LeetCode_4.java
+++ LeetCodePro/src/club/vann/leetcode/offer/LeetCode_4.java
@@ -42,13 +42,53 @@ public class LeetCode_4 {
 
 	public static void main(String[] args) {
 		LeetCode_4 leetCode = new LeetCode_4();
+		boolean result = false;
 		
-		int[][] matrix = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17,24}, {18, 21, 23, 26, 30}};
-		boolean result = leetCode.findNumberIn2DArray(matrix, 5);
+//		int[][] matrix = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17,24}, {18, 21, 23, 26, 30}};
+//		result = leetCode.findNumberIn2DArray(matrix, 5);
+//		System.out.println("Result:[true] : " + result);
+		
+//		int[][] matrix = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17,24}, {18, 21, 23, 26, 30}};
+//		result = leetCode.findNumberIn2DArray(matrix, 20);
+//		System.out.println("Result:[false] : " + result);
+//		
+//		
+//		int[][] matrix1 = {{-1, 3}};
+//		result = leetCode.findNumberIn2DArray(matrix1, 3);
+//		System.out.println("Result:[true] : " + result);
+//		
+//		int[][] matrix2 = {{-1, 3, 4}};
+//		result = leetCode.findNumberIn2DArray(matrix2, 4);
+//		System.out.println("Result:[true] : " + result);
+//		
+//		int[][] matrix3 = {{-1, 4, 5}};
+//		result = leetCode.findNumberIn2DArray(matrix3, 3);
+//		System.out.println("Result:[false] : " + result);
+//		
+//		int[][] matrix4 = {{}};
+//		result = leetCode.findNumberIn2DArray(matrix4, 1);
+//		System.out.println("Result:[false] : " + result);
+		
+//		int[][] matrix5 = {{-1}, {1}, {3}};
+//		result = leetCode.findNumberIn2DArray(matrix5, 3);
+//		System.out.println("Result:[true] : " + result);
+		
+//		int[][] matrix6 = {{1,4}, {2,5}};
+//		result = leetCode.findNumberIn2DArray(matrix6, 4);
+//		System.out.println("Result:[true] : " + result);
+		
+		int[][] matrix7 = {{1,2,3,4,5}, {6,7,8,9,10}, {11,12,13,14,15}, {16,17,18,19,20}, {21,22,23,24,25}};
+		result = leetCode.findNumberIn2DArray(matrix7, 15);
 		System.out.println("Result:[true] : " + result);
 		
-		result = leetCode.findNumberIn2DArray(matrix, 20);
-		System.out.println("Result:[false] : " + result);
+//		int[][] matrix8 = {{-5}};
+//		result = leetCode.findNumberIn2DArray(matrix8, -10);
+//		System.out.println("Result:[false] : " + result);
+//		
+//		int[][] matrix9 = {{1,2,3,4,5}, {6,7,8,9,10}, {11,12,13,14,15}, {16,17,18,19,20}, {21,22,23,24,25}};
+//		result = leetCode.findNumberIn2DArray(matrix9, 5);
+//		System.out.println("Result:[true] : " + result);
+		
 	}
 	
 	/**
@@ -65,7 +105,7 @@ public class LeetCode_4 {
 		//解法一：暴力解法，递归方法判断（LeetCode测试后超时）
 //		return findNumberIn2DArray1(matrix, target, 0, 0);
 		
-		return findNumberIn2DArray2(matrix, target);
+		return findNumberIn2DArray2(matrix, target, 0, 0, matrix[0].length, matrix.length);
     }
 	
 	private boolean findNumberIn2DArray1(int[][] matrix, int target, int xIndex, int yIndex) {
@@ -86,11 +126,35 @@ public class LeetCode_4 {
 	
 	/**
 	 * 解法二：
+	 *       
 	 * @param matrix
 	 * @param target
 	 * @return
 	 */
-	private boolean findNumberIn2DArray2(int[][] matrix, int target) {
+	private boolean findNumberIn2DArray2(int[][] matrix, int target, int x, int y, int max_x, int max_y) {
+		int point_x = matrix[0].length-1;
+		int point_y = matrix.length-1;
+		
+		while(x >= 0 && x < max_x && y >= 0 && y < max_y) {
+			int value = matrix[y][x];
+			
+			if(value == target) {
+				return true;
+			} else if (value < target) {
+				if(max_x - x < max_y - y) {
+					y ++;
+				} else if(max_x - x == max_y - y) {
+					x ++;
+					y ++;
+				} else {
+					x ++;
+				}
+				
+			} else {
+				return findNumberIn2DArray2(matrix, target, x, 0, point_x, y-1) || findNumberIn2DArray2(matrix, target, 0, y, x-1, point_y);
+			}
+		}
+		
 		return false;
 	}
 
