3a2a34963b603691552818f44187efd2c7d2e9a5
He1senber9 <fanyun2011@aliyun.com>
2020-03-08 09:50:10 +0800

    LLeetCode_322初始代码提交

---
+++ LeetCodePro/src/club/vann/leetcode/offer/daily/LeetCode_322.java
@@ -0,0 +1,40 @@
+package club.vann.leetcode.offer.daily;
+
+/**
+ * <p>
+ * 难度：Midum
+ * <p>
+ * 题目：零钱兑换
+ * <p>
+ * 描述：给定不同面额的硬币 coins 和一个总金额
+ * amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+ * 
+ * <p>
+ * 示例 1:
+ * 
+ * 输入: coins = [1, 2, 5], amount = 11 输出: 3 
+ * 解释: 11 = 5 + 5 + 1 
+ * <p>
+ * 示例 2:
+ * 
+ * 输入: coins = [2], amount = 3 输出: -1 
+ * 
+ * <p>
+ * 说明: 你可以认为每种硬币的数量是无限的。
+ * 
+ * 通过次数47,144提交次数124,202
+ * 
+ * @author fanyu
+ * @date 2020年3月8日
+ */
+public class LeetCode_322 {
+
+	public static void main(String[] args) {
+
+	}
+	
+	private int coinChange(int[] coins, int amount) {
+		return 0;
+    }
+
+}
