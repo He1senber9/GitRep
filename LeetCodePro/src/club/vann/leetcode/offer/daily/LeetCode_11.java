3b401ba57eceb889bb0a8c3533b73c081ccd80e6
He1senber9 <fanyun2011@aliyun.com>
2020-04-18 10:17:04 +0800

    LeetCode_11暴力破击法提交

--- LeetCodePro/src/club/vann/leetcode/offer/daily/LeetCode_11.java
+++ LeetCodePro/src/club/vann/leetcode/offer/daily/LeetCode_11.java
@@ -28,16 +28,31 @@ public class LeetCode_11 {
     public static void main(String[] args) {
         LeetCode_11 leetCode = new LeetCode_11();
 
-
+        System.out.println("Result["+TestCase.ans+"] : " + leetCode.maxArea(TestCase.height));
     }
 
     /**
      * 解法一：暴力破解
+     * 时间复杂度：O(n²)
+     * 空间复杂度：O(1)
+     * 采用双指针，依次找到每个边界上能盛水的容量，然后找到最大值。
      * @param height
      * @return
      */
     public int maxArea(int[] height) {
-        return -1;
+        int len = height.length;
+
+        int max = 0;
+        int left = 0, right = 1; // 因为 n >= 2;
+        for(int n = 0; n < len-1; n ++) {
+            left = n;
+            for(int m = n+1; m < len; m ++) {
+                right = m;
+                int h = Math.min(height[left], height[right]);
+                max = Math.max(max, (right-left) * h);
+            }
+        }
+        return max;
     }
 
     static class TestCase {
