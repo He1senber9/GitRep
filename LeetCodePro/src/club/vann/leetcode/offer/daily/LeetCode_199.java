f2dcbd32f808fa1fb5dfd0cf9b2f58985fdce0f4
He1senber9 <fanyun2011@aliyun.com>
2020-04-22 16:42:00 +0800

    LeetCode_199解法一代码提交

--- LeetCodePro/src/club/vann/leetcode/offer/daily/LeetCode_199.java
+++ LeetCodePro/src/club/vann/leetcode/offer/daily/LeetCode_199.java
@@ -2,8 +2,11 @@ package club.vann.leetcode.offer.daily;
 
 import club.vann.leetcode.common.TreeNode;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * <p>难度：Medium</p>
@@ -38,18 +41,49 @@ public class LeetCode_199 {
         LeetCode_199 leetCode = new LeetCode_199();
 
         System.out.println(leetCode.rightSideView(TestCase.root()));
+        System.out.println(leetCode.rightSideView(TestCase.root1()));
+        System.out.println(leetCode.rightSideView(TestCase.root2()));
     }
 
     /**
      * 解法一：
-     * 
+     * 采用中序遍历
      * @param root
      * @return
      */
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> result = new ArrayList<Integer>();
+        List<Integer> list = new ArrayList<Integer>();
+        LinkedList<TreeNode> curList = new LinkedList<TreeNode>();
 
-        return result;
+        int psize = 0;
+        int csize = 0;
+        curList.add(root);
+        psize = 1;
+        while(!curList.isEmpty()) {
+            TreeNode node = curList.pollFirst();
+
+            if(psize == 1) {
+                list.add(node.val);
+            }
+            psize --;
+
+            if(node.left != null) {
+                curList.addLast(node.left);
+                csize ++;
+            }
+
+            if(node.right != null) {
+                curList.addLast(node.right);
+                csize ++;
+            }
+
+            if(psize == 0) {
+                psize = csize;
+                csize = 0;
+            }
+        }
+
+        return list;
     }
 
     static class TestCase {
@@ -77,8 +111,35 @@ public class LeetCode_199 {
 
             root.left = node2;
             root.right = node3;
+            node2.left = node4;
             node2.right = node5;
-            node3.right = node4;
+
+            return root;
+        }
+
+        public static TreeNode root2() {
+            TreeNode root = new TreeNode(1);
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+            TreeNode node4 = new TreeNode(4);
+            TreeNode node5 = new TreeNode(5);
+            TreeNode node6 = new TreeNode(6);
+            TreeNode node7 = new TreeNode(7);
+            TreeNode node8 = new TreeNode(8);
+            TreeNode node9 = new TreeNode(9);
+            TreeNode node10 = new TreeNode(10);
+            TreeNode node11= new TreeNode(11);
+
+            root.left = node2;
+            root.right = node3;
+            node2.left = node4;
+            node2.right = node5;
+            node3.left = node6;
+            node4.left = node9;
+            node6.left = node7;
+            node6.right = node8;
+            node9.left = node10;
+            node9.right = node11;
 
             return root;
         }
