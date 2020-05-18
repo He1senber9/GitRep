91100c24c5d8df5c6e6c9c4954d612545be1ac2c
He1senber9 <fanyun2011@aliyun.com>
2020-05-19 06:34:38 +0800

    LeetCode_680初始代码提交

---
+++ LeetCodePro/src/club/vann/leetcode/offer/daily/LeetCode_680.java
@@ -0,0 +1,25 @@
+package club.vann.leetcode.offer.daily;
+
+/**
+ * @program: GitRep
+ * @description:
+ * @author: vann
+ * @create: 2020-05-19 06:32
+ **/
+public class LeetCode_680 {
+    public static void main(String[] args) {
+
+    }
+
+    private boolean validPalindrome(String s) {
+        return false;
+    }
+
+    static class TestCase {
+        public static final boolean ans = true;
+        public static final String str = "aba";
+
+        public static final boolean ans1 = true;
+        public static final String str1 = "abca";
+    }
+}
