e6272e912ad7071a0b67e31e7e62d671f8d2c14a
He1senber9 <fanyun2011@aliyun.com>
2020-06-05 17:03:25 +0800

    LeetCode_29代码完成

--- LeetCodePro/src/club/vann/leetcode/offer/daily/LeetCode_29.java
+++ LeetCodePro/src/club/vann/leetcode/offer/daily/LeetCode_29.java
@@ -1,5 +1,7 @@
 package club.vann.leetcode.offer.daily;
 
+import java.util.Arrays;
+
 /**
  * <p>难度：Easy</p>
  * <p>题目：顺时针打印矩阵</p>
@@ -35,8 +37,8 @@ public class LeetCode_29 {
     public static void main(String[] args) {
         LeetCode_29 leetCode = new LeetCode_29();
 
-        System.out.println("Result["+TestCase.ans+"] : " + leetCode.spiralOrder(TestCase.matrix));
-        System.out.println("Result["+TestCase.ans1+"] : " + leetCode.spiralOrder(TestCase.matrix1));
+        System.out.println("Result["+ Arrays.toString(TestCase.ans)+"] : " + Arrays.toString(leetCode.spiralOrder(TestCase.matrix)));
+        System.out.println("Result["+Arrays.toString(TestCase.ans1)+"] : " + Arrays.toString(leetCode.spiralOrder(TestCase.matrix1)));
     }
 
     /**
@@ -45,7 +47,41 @@ public class LeetCode_29 {
      * @return
      */
     public int[] spiralOrder(int[][] matrix) {
-        return null;
+        if(matrix == null || matrix.length == 0) {
+            return new int[0];
+        }
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+
+        // 采用一个辅助矩阵，用于判断当前元素是否被访问过
+        int[][] visited = new int[rows][columns];
+
+        // 用于控制顺时针方向
+        int[][] dr = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+
+        // 结果数组
+        int[] res = new int[rows*columns];
+
+        int y = 0, x = -1;
+        int t = 0;
+        for(int n = 0; n < rows*columns; n ++) {
+            int tmpY = y+dr[t][0];
+            int tmpX = x+dr[t][1];
+
+            if(tmpY>=0 && tmpY<rows && tmpX>=0 && tmpX<columns && visited[tmpY][tmpX] == 0) {
+                y = tmpY;
+                x = tmpX;
+                visited[y][x] = 1;
+                res[n] = matrix[y][x];
+            } else {
+                t ++;
+                t = t % 4;
+                n--;
+            }
+
+        }
+
+        return res;
     }
 
     static class TestCase {
@@ -54,5 +90,11 @@ public class LeetCode_29 {
 
         public static final int[] ans1 = {1,2,3,4,8,12,11,10,9,5,6,7};
         public static final int[][] matrix1 = {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}};
+
+        public static final int[] ans2 = {1,2,3,4,8,12,11,10,9,5,6,7};
+        public static final int[][] matrix2 = {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}, {13,14,15,16}};
+
+        public static final int[] ans3 = {1,2,3,6,9,12,11,10,7,4,5,8};
+        public static final int[][] matrix3 = {{1,2,3}, {4,5,6}, {7,8,9}, {10,11,12}};
     }
 }
