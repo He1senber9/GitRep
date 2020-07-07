908e8591329d349039acfde541e3cc9aa1696ca3
He1senber9 <fanyun2011@aliyun.com>
2020-07-07 14:42:24 +0800

    LeetCode_112解法一代码提交

--- LeetCodePro/src/club/vann/leetcode/offer/daily/LeetCode_112.java
+++ LeetCodePro/src/club/vann/leetcode/offer/daily/LeetCode_112.java
@@ -4,7 +4,7 @@ import club.vann.leetcode.common.TreeNode;
 
 /**
  * <p>难度：Easy</p>
- * <p>题目：路劲总和</p>
+ * <p>题目：路径总和</p>
  * <p>描述：给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
  *
  * 说明: 叶子节点是指没有子节点的节点。
@@ -38,8 +38,11 @@ public class LeetCode_112 {
         LeetCode_112 leetCode = new LeetCode_112();
 
         System.out.println("Result["+TestCase.ANS+"] : " + leetCode.hasPathSum(TestCase.ROOT, 22));
+        System.out.println("Result["+TestCase.ANS1+"] : " + leetCode.hasPathSum(TestCase.ROOT1, 1));
+        System.out.println("Result["+TestCase.ANS2+"] : " + leetCode.hasPathSum(TestCase.ROOT2, 0));
     }
 
+    static int num = 0;
     /**
      * 解法一：
      *
@@ -48,11 +51,43 @@ public class LeetCode_112 {
      * @return
      */
     private boolean hasPathSum(TreeNode root, int sum) {
-        return false;
+        return preOrder(root, sum, 0);
+    }
+
+
+
+    /**
+     * 返回当前节点的路径和。
+     *
+     * @param node
+     * @param sum
+     * @return
+     */
+    private boolean preOrder(TreeNode node, int sum, int tag) {
+        if(node == null) {
+            return false;
+        }
+
+        // 如果当前是叶子节点
+        if(node.left == null && node.right == null) {
+            if(tag+node.val == sum) {
+                return true;
+            }
+            return false;
+        }
+
+        tag += node.val;
+        return preOrder(node.left, sum, tag) || preOrder(node.right, sum, tag);
     }
 
     static class TestCase {
         public static final boolean ANS = true;
         public static TreeNode ROOT = TreeNode.deserialize("[5,4,8,11,null,13,4,7,2,null,null,null,1]");
+
+        public static final boolean ANS1 = true;
+        public static TreeNode ROOT1 = TreeNode.deserialize("[1]");
+
+        public static final boolean ANS2 = false;
+        public static TreeNode ROOT2 = TreeNode.deserialize("[1,2]");
     }
 }
