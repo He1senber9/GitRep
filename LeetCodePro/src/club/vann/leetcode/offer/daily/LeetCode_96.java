c4db428fb4d38e7359dcc5cc325d9abf37b0a28b
He1senber9 <fanyun2011@aliyun.com>
2020-07-15 22:02:59 +0800

    LeetCode_96代码提交

--- LeetCodePro/src/club/vann/leetcode/offer/daily/LeetCode_96.java
+++ LeetCodePro/src/club/vann/leetcode/offer/daily/LeetCode_96.java
@@ -43,7 +43,27 @@ public class LeetCode_96 {
      * @return
      */
     private int numTrees(int n) {
-        return 0;
+        if(n<=2) {
+            return n;
+        }
+        int[] G = new int[n+1];
+        G[0] = 1;
+        G[1] = 1;
+        for(int i = 2; i <= n; i ++) {
+            for(int j = 1; j <= i; j ++) {
+                G[i] += G[j-1] * G[i-j];
+            }
+        }
+        return G[n];
+    }
+
+    private int numTrees1(int n) {
+        // 提示：我们在这里需要用 long 类型防止计算过程中的溢出
+        long C = 1;
+        for (int i = 0; i < n; ++i) {
+            C = C * 2 * (2 * i + 1) / (i + 2);
+        }
+        return (int) C;
     }
 
     static class TestCase {
