9b4fe07e15a98dfe608286fb1305862b2f236660
He1senber9 <fanyun2011@aliyun.com>
2020-07-23 09:42:32 +0800

    LeetCode_64_II代码提交

---
+++ LeetCodePro/src/club/vann/leetcode/offer/daily/LeetCode_64_II.java
@@ -0,0 +1,46 @@
+package club.vann.leetcode.offer.daily;
+
+/**
+ * @program: Tailbase
+ * @description:
+ * @author: vann
+ * @create: 2020-07-23 08:54
+ **/
+public class LeetCode_64_II {
+    public static void main(String[] args) {
+        LeetCode_64_II leetCode = new LeetCode_64_II();
+
+        System.out.println("Result["+TestCase.ANS+"] : " + leetCode.minPathSum(TestCase.GRID));
+    }
+
+    /**
+     * 解法一：
+     *
+     * @param grid
+     * @return
+     */
+    public int minPathSum(int[][] grid) {
+        int lenY = grid.length;
+        int lenX = grid[0].length;
+        for(int y = 0; y < lenY; y ++) {
+            for(int x = 0; x < lenX; x ++) {
+                if(y==0 && x == 0) {
+
+                } else if(y>=1 && x ==0) {
+                    grid[y][0] = grid[y-1][0]+grid[y][x];
+                } else if(x>=1 && y == 0) {
+                    grid[0][x] = grid[0][x-1]+grid[y][x];
+                } else {
+                    grid[y][x] = Math.min(grid[y-1][x], grid[y][x-1]) + grid[y][x];
+                }
+            }
+
+        }
+        return grid[lenY-1][lenX-1];
+    }
+
+    static class TestCase {
+        public static int ANS = 7;
+        public static int[][] GRID = {{1,3,1},{1,5,1},{4,2,1}};
+    }
+}
