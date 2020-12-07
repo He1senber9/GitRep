390686d0d9003e2b6cefe57a8b41d7f0f06fca2c
He1senber9 <fanyun2011@aliyun.com>
2020-12-07 10:57:21 +0800

    LeetCode_861解法一代码提交

--- LeetCodePro/src/club/vann/leetcode/offer/daily/LeetCode_861.java
+++ LeetCodePro/src/club/vann/leetcode/offer/daily/LeetCode_861.java
@@ -54,11 +54,22 @@ public class LeetCode_861 {
      * @return
      */
     public int matrixScore(int[][] A) {
-        int y = A.length;
-        int x = A[0].length;
+        int m = A.length, n = A[0].length;
+        int ret = m * (1 << (n - 1));
 
-        // 尽可能的使靠近左边的列中 1 最多
-        return 0;
+        for (int j = 1; j < n; j++) {
+            int nOnes = 0;
+            for (int i = 0; i < m; i++) {
+                if (A[i][0] == 1) {
+                    nOnes += A[i][j];
+                } else {
+                    nOnes += (1 - A[i][j]); // 如果这一行进行了行反转，则该元素的实际取值为 1 - A[i][j]
+                }
+            }
+            int k = Math.max(nOnes, m - nOnes);
+            ret += k * (1 << (n - j - 1));
+        }
+        return ret;
     }
 
     static class TestCase {
