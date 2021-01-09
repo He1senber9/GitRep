0b4c70771b6dd4d72e196b144b5732151038f01d
He1senber9 <fanyun2011@aliyun.com>
2021-01-09 13:20:25 +0800

    LeetCode_188初始代码提交。

--- LeetCodePro/src/club/vann/leetcode/offer/daily/LeetCode_188.java
+++ LeetCodePro/src/club/vann/leetcode/offer/daily/LeetCode_188.java
@@ -48,12 +48,12 @@ public class LeetCode_188 {
     public static void main(String[] args) {
         LeetCode_188 leetCode = new LeetCode_188();
 
-        System.out.println("Result["+TestCase.ANS+"] : " + leetCode.maxProfit1(TestCase.K, TestCase.PRICES));
-        System.out.println("Result["+TestCase.ANS1+"] : " + leetCode.maxProfit1(TestCase.K1, TestCase.PRICES1));
-        System.out.println("Result["+TestCase.ANS2+"] : " + leetCode.maxProfit1(TestCase.K2, TestCase.PRICES2));
-        System.out.println("Result["+TestCase.ANS4+"] : " + leetCode.maxProfit1(TestCase.K4, TestCase.PRICES4));
-        System.out.println("Result["+TestCase.ANS5+"] : " + leetCode.maxProfit1(TestCase.K5, TestCase.PRICES5));
-        System.out.println("Result["+TestCase.ANS6+"] : " + leetCode.maxProfit1(TestCase.K6, TestCase.PRICES6));
+        System.out.println("Result["+TestCase.ANS+"] : " + leetCode.maxProfit2(TestCase.K, TestCase.PRICES));
+        System.out.println("Result["+TestCase.ANS1+"] : " + leetCode.maxProfit2(TestCase.K1, TestCase.PRICES1));
+        System.out.println("Result["+TestCase.ANS2+"] : " + leetCode.maxProfit2(TestCase.K2, TestCase.PRICES2));
+        System.out.println("Result["+TestCase.ANS4+"] : " + leetCode.maxProfit2(TestCase.K4, TestCase.PRICES4));
+        System.out.println("Result["+TestCase.ANS5+"] : " + leetCode.maxProfit2(TestCase.K5, TestCase.PRICES5));
+        System.out.println("Result["+TestCase.ANS6+"] : " + leetCode.maxProfit2(TestCase.K6, TestCase.PRICES6));
     }
 
     /**
@@ -121,6 +121,53 @@ public class LeetCode_188 {
             }
         }
         return pro[k-1];
+    }
+
+    /**
+     * 解法三：
+     * 动态规划
+     * @param k
+     * @param prices
+     * @return
+     */
+    public int maxProfit2(int k, int[] prices) {
+        int n = prices.length;
+        if(n <= 1 || k == 0) {
+            return 0;
+        }
+        if(k > n/2) {
+            return maxProfit(prices);
+        }
+
+        int[][][] dp = new int[n][k+1][2];
+        for(int i = 1; i <= k; i ++) {
+            dp[0][i][0] = 0;
+            dp[0][i][1] = -prices[0];
+        }
+
+        for(int i = 1; i < n; i ++) {
+            for(int j = 1; j <= k; j ++) {
+                dp[i][j][0] = Math.max(dp[i-1][j][0], dp[i-1][j][1]+prices[i]);
+                dp[i][j][1] = Math.max(dp[i-1][j][1], dp[i-1][j-1][0]-prices[i]);
+            }
+        }
+
+        return dp[n-1][k][0];
+    }
+
+    private int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int length = prices.length;
+        int[][] dp = new int[length][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[length - 1][0];
     }
 
     static class TestCase {
