df7d20dc0b43366411e4b5ad9541893b0337e6ae
He1senber9 <fanyun2011@aliyun.com>
2021-05-21 08:52:34 +0800

    LeetCode代码提交。

--- LeetCodePro/src/club/vann/leetcode/offer/daily/LeetCode_1035.java
+++ LeetCodePro/src/club/vann/leetcode/offer/daily/LeetCode_1035.java
@@ -60,13 +60,32 @@ public class LeetCode_1035 {
 
     /**
      * 解法一：
+     * 实际上就是求两个数组的最大公共子序列长度。
+     * dp[i][j] 表明为nums1的长度为i，nums2的长度为j的序列中最大公共子序列长度。
      *
+     * dp[i][j] =
+     *   1. dp[i][j] = dp[i-1][j-1] + 1 (nums1[i] == nums2[j])
+     *   2. dp[i][j] = max(dp[i-1][j], dp[i][j-1]) (nums1[i] != nums2[j])
      * @param nums1
      * @param nums2
      * @return
      */
     public int maxUncrossedLines(int[] nums1, int[] nums2) {
-        return 0;
+        int m = nums1.length;
+        int n = nums2.length;
+
+        int[][] dp = new int[m+1][n+1];
+
+        for(int i = 1; i <= m; i ++) {
+            for(int j = 1; j <= n; j ++) {
+                if(nums1[i-1] == nums2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
     }
 
     static class TestCase {
