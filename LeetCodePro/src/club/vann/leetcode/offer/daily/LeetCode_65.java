bbeffe391de287e6772c3fc5855bd1b060169481
He1senber9 <fanyun2011@163.com>
2021-06-17 11:01:06 +0800

    LeetCode代码提交。

---
+++ LeetCodePro/src/club/vann/leetcode/offer/daily/LeetCode_65.java
@@ -0,0 +1,194 @@
+package club.vann.leetcode.offer.daily;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <p>难度：Hard</p>
+ * <p>题目：有效数字</p>
+ * <p>描述：有效数字（按顺序）可以分成以下几个部分：
+ *
+ * 一个 小数 或者 整数
+ * （可选）一个 'e' 或 'E' ，后面跟着一个 整数
+ * 小数（按顺序）可以分成以下几个部分：
+ *
+ * （可选）一个符号字符（'+' 或 '-'）
+ * 下述格式之一：
+ * 至少一位数字，后面跟着一个点 '.'
+ * 至少一位数字，后面跟着一个点 '.' ，后面再跟着至少一位数字
+ * 一个点 '.' ，后面跟着至少一位数字
+ * 整数（按顺序）可以分成以下几个部分：
+ *
+ * （可选）一个符号字符（'+' 或 '-'）
+ * 至少一位数字
+ * 部分有效数字列举如下：
+ *
+ * ["2", "0089", "-0.1", "+3.14", "4.", "-.9", "2e10", "-90E3", "3e+7", "+6e-1", "53.5e93", "-123.456e789"]
+ * 部分无效数字列举如下：
+ *
+ * ["abc", "1a", "1e", "e3", "99e2.5", "--6", "-+3", "95a54e53"]
+ * 给你一个字符串 s ，如果 s 是一个 有效数字 ，请返回 true 。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：s = "0"
+ * 输出：true
+ * 示例 2：
+ *
+ * 输入：s = "e"
+ * 输出：false
+ * 示例 3：
+ *
+ * 输入：s = "."
+ * 输出：false
+ * 示例 4：
+ *
+ * 输入：s = ".1"
+ * 输出：true
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= s.length <= 20
+ * s 仅含英文字母（大写和小写），数字（0-9），加号 '+' ，减号 '-' ，或者点 '.' 。
+ * 通过次数27,843提交次数120,522
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/valid-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。</p>
+ * @description:
+ * @program: GitRep
+ * @author: fanyu
+ * @create: 2021-06-17  08:00:27
+ */
+public class LeetCode_65 {
+    public static void main(String[] args) {
+        LeetCode_65 leetCode = new LeetCode_65();
+
+        System.out.println("Result["+TestCase.ANS+"] : " + leetCode.isNumber(TestCase.S));
+        System.out.println("Result["+TestCase.ANS1+"] : " + leetCode.isNumber(TestCase.S1));
+        System.out.println("Result["+TestCase.ANS2+"] : " + leetCode.isNumber(TestCase.S2));
+        System.out.println("Result["+TestCase.ANS3+"] : " + leetCode.isNumber(TestCase.S3));
+    }
+
+    /**
+     * 解法一：
+     *
+     * @param s
+     * @return
+     */
+    public boolean isNumber(String s) {
+        Map<State, Map<CharType, State>> transfer = new HashMap<State, Map<CharType, State>>();
+        Map<CharType, State> initialMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_INTEGER);
+            put(CharType.CHAR_POINT, State.STATE_POINT_WITHOUT_INT);
+            put(CharType.CHAR_SIGN, State.STATE_INT_SIGN);
+        }};
+        transfer.put(State.STATE_INITIAL, initialMap);
+        Map<CharType, State> intSignMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_INTEGER);
+            put(CharType.CHAR_POINT, State.STATE_POINT_WITHOUT_INT);
+        }};
+        transfer.put(State.STATE_INT_SIGN, intSignMap);
+        Map<CharType, State> integerMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_INTEGER);
+            put(CharType.CHAR_EXP, State.STATE_EXP);
+            put(CharType.CHAR_POINT, State.STATE_POINT);
+        }};
+        transfer.put(State.STATE_INTEGER, integerMap);
+        Map<CharType, State> pointMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_FRACTION);
+            put(CharType.CHAR_EXP, State.STATE_EXP);
+        }};
+        transfer.put(State.STATE_POINT, pointMap);
+        Map<CharType, State> pointWithoutIntMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_FRACTION);
+        }};
+        transfer.put(State.STATE_POINT_WITHOUT_INT, pointWithoutIntMap);
+        Map<CharType, State> fractionMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_FRACTION);
+            put(CharType.CHAR_EXP, State.STATE_EXP);
+        }};
+        transfer.put(State.STATE_FRACTION, fractionMap);
+        Map<CharType, State> expMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_EXP_NUMBER);
+            put(CharType.CHAR_SIGN, State.STATE_EXP_SIGN);
+        }};
+        transfer.put(State.STATE_EXP, expMap);
+        Map<CharType, State> expSignMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_EXP_NUMBER);
+        }};
+        transfer.put(State.STATE_EXP_SIGN, expSignMap);
+        Map<CharType, State> expNumberMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_EXP_NUMBER);
+        }};
+        transfer.put(State.STATE_EXP_NUMBER, expNumberMap);
+
+        int length = s.length();
+        State state = State.STATE_INITIAL;
+
+        for (int i = 0; i < length; i++) {
+            CharType type = toCharType(s.charAt(i));
+            if (!transfer.get(state).containsKey(type)) {
+                return false;
+            } else {
+                state = transfer.get(state).get(type);
+            }
+        }
+        return state == State.STATE_INTEGER || state == State.STATE_POINT || state == State.STATE_FRACTION || state == State.STATE_EXP_NUMBER || state == State.STATE_END;
+    }
+
+    public CharType toCharType(char ch) {
+        if (ch >= '0' && ch <= '9') {
+            return CharType.CHAR_NUMBER;
+        } else if (ch == 'e' || ch == 'E') {
+            return CharType.CHAR_EXP;
+        } else if (ch == '.') {
+            return CharType.CHAR_POINT;
+        } else if (ch == '+' || ch == '-') {
+            return CharType.CHAR_SIGN;
+        } else {
+            return CharType.CHAR_ILLEGAL;
+        }
+    }
+
+    enum State {
+        STATE_INITIAL,
+        STATE_INT_SIGN,
+        STATE_INTEGER,
+        STATE_POINT,
+        STATE_POINT_WITHOUT_INT,
+        STATE_FRACTION,
+        STATE_EXP,
+        STATE_EXP_SIGN,
+        STATE_EXP_NUMBER,
+        STATE_END
+    }
+
+    enum CharType {
+        CHAR_NUMBER,
+        CHAR_EXP,
+        CHAR_POINT,
+        CHAR_SIGN,
+        CHAR_ILLEGAL
+    }
+
+    static class TestCase {
+        public static boolean ANS = true;
+        public static String S = "0";
+
+        public static boolean ANS1 = false;
+        public static String S1 = "e";
+
+        public static boolean ANS2 = false;
+        public static String S2 = ".";
+
+        public static boolean ANS3 = true;
+        public static String S3 = ".1";
+
+        public static boolean ANS4 = true;
+        public static String S4 = "0";
+    }
+}
