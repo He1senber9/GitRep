a307c0e68f6831d86e0fe4f04ec1fd5b3a801cb4
He1senber9 <fanyun2011@163.com>
2021-06-26 08:00:23 +0800

    LeetCode代码提交。

---
+++ LeetCodePro/src/club/vann/leetcode/offer/daily/LeetCode_773.java
@@ -0,0 +1,143 @@
+package club.vann.leetcode.offer.daily;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
+/**
+ * <p>难度：Hard</p>
+ * <p>题目：滑动谜题</p>
+ * <p>描述：在一个 2 x 3 的板上（board）有 5 块砖瓦，用数字 1~5 来表示, 以及一块空缺用 0 来表示.
+ *
+ * 一次移动定义为选择 0 与一个相邻的数字（上下左右）进行交换.
+ *
+ * 最终当板 board 的结果是 [[1,2,3],[4,5,0]] 谜板被解开。
+ *
+ * 给出一个谜板的初始状态，返回最少可以通过多少次移动解开谜板，如果不能解开谜板，则返回 -1 。
+ *
+ * 示例：
+ *
+ * 输入：board = [[1,2,3],[4,0,5]]
+ * 输出：1
+ * 解释：交换 0 和 5 ，1 步完成
+ * 输入：board = [[1,2,3],[5,4,0]]
+ * 输出：-1
+ * 解释：没有办法完成谜板
+ * 输入：board = [[4,1,2],[5,0,3]]
+ * 输出：5
+ * 解释：
+ * 最少完成谜板的最少移动次数是 5 ，
+ * 一种移动路径:
+ * 尚未移动: [[4,1,2],[5,0,3]]
+ * 移动 1 次: [[4,1,2],[0,5,3]]
+ * 移动 2 次: [[0,1,2],[4,5,3]]
+ * 移动 3 次: [[1,0,2],[4,5,3]]
+ * 移动 4 次: [[1,2,0],[4,5,3]]
+ * 移动 5 次: [[1,2,3],[4,5,0]]
+ * 输入：board = [[3,2,4],[1,5,0]]
+ * 输出：14
+ * 提示：
+ *
+ * board 是一个如上所述的 2 x 3 的数组.
+ * board[i][j] 是一个 [0, 1, 2, 3, 4, 5] 的排列.
+ * 通过次数9,873提交次数14,992
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/sliding-puzzle
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。</p>
+ * @description:
+ * @program: GitRep
+ * @author: fanyu
+ * @create: 2021-06-26  07:47:11
+ */
+public class LeetCode_773 {
+    public static void main(String[] args) {
+        LeetCode_773 leetCode = new LeetCode_773();
+
+        System.out.println("Result["+TestCase.ANS+"] : " + leetCode.slidingPuzzle(TestCase.BOARD));
+        System.out.println("Result["+TestCase.ANS1+"] : " + leetCode.slidingPuzzle(TestCase.BOARD1));
+        System.out.println("Result["+TestCase.ANS2+"] : " + leetCode.slidingPuzzle(TestCase.BOARD2));
+        System.out.println("Result["+TestCase.ANS3+"] : " + leetCode.slidingPuzzle(TestCase.BOARD3));
+    }
+
+    int[][] neighbors = {{1, 3}, {0, 2, 4}, {1, 5}, {0, 4}, {1, 3, 5}, {2, 4}};
+    /**
+     * 解法一：
+     *
+     * @param board
+     * @return
+     */
+    public int slidingPuzzle(int[][] board) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < 2; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                sb.append(board[i][j]);
+            }
+        }
+        String initial = sb.toString();
+        if ("123450".equals(initial)) {
+            return 0;
+        }
+
+        int step = 0;
+        Queue<String> queue = new LinkedList<String>();
+        queue.offer(initial);
+        Set<String> seen = new HashSet<String>();
+        seen.add(initial);
+
+        while (!queue.isEmpty()) {
+            ++step;
+            int size = queue.size();
+            for (int i = 0; i < size; ++i) {
+                String status = queue.poll();
+                for (String nextStatus : get(status)) {
+                    if (!seen.contains(nextStatus)) {
+                        if ("123450".equals(nextStatus)) {
+                            return step;
+                        }
+                        queue.offer(nextStatus);
+                        seen.add(nextStatus);
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    // 枚举 status 通过一次交换操作得到的状态
+    public List<String> get(String status) {
+        List<String> ret = new ArrayList<String>();
+        char[] array = status.toCharArray();
+        int x = status.indexOf('0');
+        for (int y : neighbors[x]) {
+            swap(array, x, y);
+            ret.add(new String(array));
+            swap(array, x, y);
+        }
+        return ret;
+    }
+
+    public void swap(char[] array, int x, int y) {
+        char temp = array[x];
+        array[x] = array[y];
+        array[y] = temp;
+    }
+
+    static class TestCase {
+        public static int ANS = 1;
+        public static int[][] BOARD = {{1, 2, 3}, {4, 0, 5}};
+
+        public static int ANS1 = -1;
+        public static int[][] BOARD1 = {{1, 2, 3}, {5, 4, 0}};
+
+        public static int ANS2 = 5;
+        public static int[][] BOARD2 = {{4, 1, 2}, {5, 0, 3}};
+
+        public static int ANS3 = 14;
+        public static int[][] BOARD3 = {{3, 2, 4}, {1, 5, 0}};
+    }
+}
