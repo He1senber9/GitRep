5cdfc1721dc5be3c436bf5c15e4b1cf0899ed12c
He1senber9 <fanyun2011@163.com>
2021-06-27 16:53:23 +0800

    LeetCode代码提交。

---
+++ LeetCodePro/src/club/vann/leetcode/offer/daily/LeetCode_909.java
@@ -0,0 +1,74 @@
+package club.vann.leetcode.offer.daily;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
+/**
+ * @description:
+ * @program: GitRep
+ * @author: fanyu
+ * @create: 2021-06-27  08:16:51
+ */
+public class LeetCode_909 {
+    public static void main(String[] args) {
+        LeetCode_909 leetCode = new LeetCode_909();
+
+        System.out.println("Result["+TestCase.ANS+"] : " + leetCode.snakesAndLadders(TestCase.BOARD));
+    }
+
+    /**
+     * 解法一：
+     *
+     * @param board
+     * @return
+     */
+    public int snakesAndLadders(int[][] board) {
+        int n = board.length;
+        boolean[] vis = new boolean[n * n + 1];
+        Queue<int[]> queue = new LinkedList<int[]>();
+        queue.offer(new int[]{1, 0});
+        while (!queue.isEmpty()) {
+            int[] p = queue.poll();
+            for (int i = 1; i <= 6; ++i) {
+                int nxt = p[0] + i;
+                if (nxt > n * n) { // 超出边界
+                    break;
+                }
+                int[] rc = id2rc(nxt, n); // 得到下一步的行列
+                if (board[rc[0]][rc[1]] > 0) { // 存在蛇或梯子
+                    nxt = board[rc[0]][rc[1]];
+                }
+                if (nxt == n * n) { // 到达终点
+                    return p[1] + 1;
+                }
+                if (!vis[nxt]) {
+                    vis[nxt] = true;
+                    queue.offer(new int[]{nxt, p[1] + 1}); // 扩展新状态
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int[] id2rc(int id, int n) {
+        int r = (id - 1) / n, c = (id - 1) % n;
+        if (r % 2 == 1) {
+            c = n - 1 - c;
+        }
+        return new int[]{n - 1 - r, c};
+    }
+
+    static class TestCase {
+        public static int ANS = 4;
+        public static int[][] BOARD = {{-1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1},
+                {-1, 35, -1, -1, 13, -1},
+                {-1, -1, -1, -1, -1, -1},
+                {-1, 15, -1, -1, -1, -1}};
+    }
+}
